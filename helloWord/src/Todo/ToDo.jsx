d87ca4d317cb4a4023afb728cdb61fe6cbec6252
MarCrivelli <marcoantonio7crivelli@gmail.com>
2023-09-03 18:23:15 -0300

    Update 19
    Correção de erros

--- helloWord/src/Todo/ToDo.jsx
+++ helloWord/src/Todo/ToDo.jsx
@@ -11,10 +11,6 @@ export default function UparImagem() {
     const [lista, setLista] = useState([]);
 
 
-    //Após isso a lista é atualizada pela função "setLista". O conteúdo da lista está sendo salvo e em seguida é adicionado um novo trecho como o novo valor de selecionarImagem (valor esse que o usuário coloca no "input") 
-    setLista([...lista, {selecionarImagem:selecionarImagem}]);
-
-
     // A função "procurarArquivo" vai ser chamada quando o usuário escolher uma imagem
     const procurarArquivo = (e) => {
 
@@ -28,9 +24,9 @@ export default function UparImagem() {
 
             //Se um arquivo foi selecionado, a função "URL.createObjectURL" vai criar uma URL para meio que "representar" o arquivo que vai ser selecionado. A mesma é atribuida a variável "arquivoSelecionado" usando setSelecionarImagem, o que faz com que a imagem seja exibida na página.
             const imagemURL = URL.createObjectURL(arquivoSelecionado);
-            setSelecionarImagem(imagemURL)
+            setSelecionarImagem(imagemURL);
         }
-    }
+    };
 
     const salvar = () => {
         if (selecionarImagem){
@@ -60,16 +56,15 @@ export default function UparImagem() {
                     className="imagemSelecionada"
                     style={{
                         maxWidth: selecionarImagem.width > selecionarImagem.height ? "400px" : "300px",
-                        maxHeight: selecionarImagem.width > selecionarImagem.height ? "300px" : "400px",
+                        maxHeight: selecionarImagem.width > selecionarImagem.height ? "300px" : "400px"
                     }}
-                    ></img>
+                    />
                     <button onClick={salvar}>Salvar</button>
                     <button onClick={() => removerImagem(index)}>Remover</button>
                   </div>
                 )}
             </form>
-        
-        
+            <div>
                 {lista.map((imagemURL, index) => (
                  <div key={index}>
                      <img
@@ -77,12 +72,14 @@ export default function UparImagem() {
                      alt={`Imagem ${index + 1}`}   
                      className="imagemSelecionada"
                      style={{
-                        maxWidth: selecionarImagem.width > selecionarImagem.height ? "400px" : "300px",
-                        maxHeight: selecionarImagem.width > selecionarImagem.height ? "300px" : "400px",
+                        maxWidth: imagemURL.width > imagemURL.height ? "400px" : "300px",
+                        maxHeight: imagemURL.width > imagemURL.height ? "300px" : "400px"
                     }}
-                     />
+                    />
+                    <button onClick={() => removerImagem(index)}>Remover</button>
                 </div>
-            ))}
+                ))}
+            </div>
         </div>
         //O map vai percorrer cada item da lista. Isso vai fazer com que, para cada item, seja criado um parágrafo.
     );
